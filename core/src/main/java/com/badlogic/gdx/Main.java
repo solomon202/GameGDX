6566c5246f485280e313af3b1cb16b6a2c550f81
solomon202 <vivo5757@yandex.ru>
2026-02-10 01:35:35 +0300

    two

--- core/src/main/java/com/badlogic/gdx/Main.java
+++ core/src/main/java/com/badlogic/gdx/Main.java
@@ -1,32 +1,168 @@
 package com.badlogic.gdx;
-
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
+import com.badlogic.gdx.utils.viewport.FitViewport;
+//набор методов, которые класс должен реализоват
+public class Main implements ApplicationListener {
+	//картинки звуки квадрат картинки обьекты 
+    Texture backgroundTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+    Sprite bucketSprite;
+    Vector2 touchPos;
+    Array<Sprite> dropSprites;
+    float dropTimer;
+    Rectangle bucketRectangle;
+    Rectangle dropRectangle;
+    //create() — вызывается один раз при создании приложения.
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+    	//Это позволяет загрузить ресурсы в память после запуска
+        backgroundTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(9, 7);
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1, 1);
+        touchPos = new Vector2();
+        dropSprites = new Array<>();
+        bucketRectangle = new Rectangle();
+        dropRectangle = new Rectangle();
+        music.setLooping(true);
+        music.setVolume(.5f);
+        music.play();
+    }
+  //Вызывается один раз сразу после метода create() размер фоновой  картинки 
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+//render() выполняется 60 раз в секунду 
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+    	//скорость передвижения корзины 
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
+  //если нажата кнопка то это 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        	//товызывается токойто спрайт с такойто скоростью по горизонтале 
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
+ //мышка
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+        }
+    }
+
+    private void logic() {
+    	//получить ширену высоту фона 
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        ////получить ширену высоту ведра
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+        //чтобы ведро не смещалось за края 
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+     //получить текущую дельту
+        float delta = Gdx.graphics.getDeltaTime();
+        //размер ведра и его позицыя переводим в квадрат 
+        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+
+        for (int i = dropSprites.size - 1; i >= 0; i--) {
+            Sprite dropSprite = dropSprites.get(i);
+            float dropWidth = dropSprite.getWidth();
+            float dropHeight = dropSprite.getHeight();
+
+            dropSprite.translateY(-2f * delta);
+            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+
+            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
+            else if (bucketRectangle.overlaps(dropRectangle)) {
+                dropSprites.removeIndex(i);
+                dropSound.play();
+            }
+        }
+
+        dropTimer += delta;
+        if (dropTimer > 1f) {
+            dropTimer = 0;
+            createDroplet();
+        }
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        bucketSprite.draw(spriteBatch);
+
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.draw(spriteBatch);
+        }
+
+        spriteBatch.end();
+    }
+
+    private void createDroplet() {
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidth, dropHeight);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
     }
 
     @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+        
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        
     }
 }
